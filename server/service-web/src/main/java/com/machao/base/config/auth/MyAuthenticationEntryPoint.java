b0ddf0c6db94d356384d10ace33d93aeac812b95
machao <cma2@cisco.com>
2018-04-16 14:09:39 +0800

    add basic auth when access swagger-ui

--- server/service-web/src/main/java/com/machao/base/config/auth/MyAuthenticationEntryPoint.java
+++ server/service-web/src/main/java/com/machao/base/config/auth/MyAuthenticationEntryPoint.java
@@ -20,6 +20,8 @@ public class MyAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
+		if(request.getRequestURI().contains("swagger-ui.html"))
+			response.setHeader("WWW-authenticate", "Basic Realm=\"Please Login\"");
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication Failed: " + authException.getMessage());  
 	}
 	
